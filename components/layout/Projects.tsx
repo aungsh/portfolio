c357f96a22d1a497c9f7d3be28e47536ac24d255
Aung <74162870+aungsh@users.noreply.github.com>
2025-11-04 00:45:38 +0800

    add sit aio project

--- components/layout/Projects.tsx
+++ components/layout/Projects.tsx
@@ -11,6 +11,12 @@ interface Project {
 export default function Projects() {
   const projects: Project[] = [
     {
+      title: "SIT-AIO (Work in Progress)",
+      description: "Classroom assistant for SIT students.",
+      techStack: ["Arduino"],
+      url: "https://sit-aio.vercel.app/",
+    },
+    {
       title: "Popcorn Vibes",
       description: "Sentiment analysis for movie reviews.",
       techStack: ["Next.js", "Python", "Fast API"],
@@ -35,8 +41,8 @@ export default function Projects() {
       <h2 className="text-sm text-secondary font-black">Projects</h2>
       <div className="mt-4 space-y-6">
         {projects.map(({ title, description, techStack, url }) => (
-          <div className="">
-            <a key={title} href={url} target="_blank" rel="noopener noreferrer">
+          <div key={title}>
+            <a href={url} target="_blank" rel="noopener noreferrer">
               <h3 className="font-bold">{title}</h3>
               <p className="text-muted-foreground mb-3">{description}</p>
               <div className="flex flex-wrap gap-2">
