1afd0b04b70deb15e83f80319356d74ef7c696bc
Aung <74162870+aungsh@users.noreply.github.com>
2026-03-13 20:25:42 +0800

    Update now

--- components/layout/Now.tsx
+++ components/layout/Now.tsx
@@ -6,9 +6,7 @@ export default function Now() {
 
         <div className="mt-4">
           First-year student at the Singapore Institute of Technology, focused
-          on growing both in and outside the classroom. Lately, I have been
-          learning new technologies and training with SIT Dragon Boat to
-          strengthen my body and mindset.
+          on growing both in and outside the classroom.
         </div>
       </section>
     </>
